a666803b83a39a4b89f00f80725fd8c88876cb4c
Suyeon Cha <suyeonc.cha@gmail.com>
2024-12-24 16:52:46 +0900

    minor layour changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import ExampleHomePage from "../components/home/ExampleHomePage";
+import ExampleHomePage from "../components/home/ExampleHomePage.js";
 
 export default function Home() {
   return (
