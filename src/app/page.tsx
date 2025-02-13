6ef37e49cc1aa3ce854e798cc12818a1631c197e
Suyeon Cha <suyeonc@andrew.cmu.edu>
2025-02-13 18:42:33 -0500

    post it effect?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <Particles />
       <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 flex flex-col ${moodCrowd.className} text-white text-[100px] font-normal leading-[30px] uppercase pl-[30px] pr-[30px] items-end drop-shadow-[0_-1px_3px_rgba(255,255,255,1)] `}>
         <Logo />
-        <p className={`text-white text-[20px] m-4 ${greyMonoTrial.className}`}> coming soon </p>
+        <p className={`text-white text-[15px] m-4 ${greyMonoTrial.className}`}> coming soon </p>
       </div>
       
     </div>
