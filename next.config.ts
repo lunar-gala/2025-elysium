f6a5312dfa0772b5f3f29d2d33246a729b1d47ac
Suyeon Cha <suyeonc@andrew.cmu.edu>
2026-02-23 10:34:08 +0900

    update next

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.glsl$/,
-      use: 'raw-loader',
-    });
-    return config;
+  turbopack: {
+    rules: {
+      "*.glsl": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+    },
   },
-  /* other config options here */
 };
-
 
 export default nextConfig;
